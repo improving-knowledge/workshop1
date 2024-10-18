637c7ebfe568576f8db4213c6fddf322f266e143
/Sergio moreno <smorenodiaz_21@hotmail.com>
2024-10-18 16:31:38 -0500

    Se adiciona ejecutable

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# config para hacer cambios dentro de variables de 
+# entorno de git en el local
+#
+echo "Hello World"
+# borrado de variables de git 
+git config --global --unset user.email
+git config --global --unset user.name
+
+# adicion de nuevas variables
+
+git config --global --add user.email mail@gmail.com
+git config --global --add user.name name-of-person
+
+# Listar variables de entorno de git local
+#
+git config --global --list
+
+echo "TO DO :) "
